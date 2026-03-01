eadf652ea7b46d6f3b2374d9b3073c128bc3b15b
zkOxdaps <106523041+xthample@users.noreply.github.com>
2026-03-01 08:30:12 +0700

    Update index.ts

--- smart-contract/src/index.ts
+++ smart-contract/src/index.ts
@@ -3,8 +3,21 @@ import { Calldata } from "@btc-vision/btc-runtime/runtime";
 
 let contract: OneBet | null = null;
 
+/**
+ * WAJIB ADA.
+ * Dipanggil saat deploy oleh OP_NET runtime.
+ */
+export function start(): void {
+  contract = new OneBet();
+}
+
+/**
+ * Dipanggil setiap transaksi.
+ */
 export function execute(calldata: Calldata): void {
-  if (contract === null) {
+  if (!contract) {
     contract = new OneBet();
   }
+
+  contract.execute(calldata);
 }
